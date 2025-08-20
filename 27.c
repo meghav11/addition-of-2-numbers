5266ba4ffabf4d5983b7ba262237bfe7d6ab4299
meghav11 <meghavpatwari11@gmail.com>
2025-08-20 11:45:21 +0530

    Add files via upload

---
+++ 27.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main() {
+
+
+    int a,b;
+    printf("ënter the first number:");
+    scanf("%d",&a);
+
+    printf("ënter the second number:");
+    scanf("%d",&b);
+    if(a>b) {
+        printf("%d is largest",a);
+    }
+    else{
+        printf("%d is largest",b);
+        }}
+
