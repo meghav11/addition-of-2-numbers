513f791369fe75b237a70be794e1b4fc12cd6abb
meghav11 <meghavpatwari11@gmail.com>
2025-09-24 11:54:46 +0530

    Add files via upload

---
+++ sum of 1 to 500 27 .c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() {
+    int i, sum = 0;
+    for(i = 1; i <= 500; i++) {
+        sum += i;
+        }
+        printf("the sum of numbers from 1 to 500 is: %d\n", sum);
+        }
