e98a6cbb055c0f15f8e36e6167f977de2cf2b8f5
meghav11 <meghavpatwari11@gmail.com>
2025-09-27 22:05:40 +0530

    Add files via upload

---
+++ divisible by 7 24.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    // Input a number
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // Check divisibility by 7
+    if (num % 7 == 0) {
+        printf("%d is divisible by 7.\n", num);
+    } else {
+        printf("%d is not divisible by 7.\n", num);
+    }
+
+    return 0;
+}
+
+
