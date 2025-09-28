b07384e7f18d40b2ce9f68669c4ba2022fc5bea8
meghav11 <meghavpatwari11@gmail.com>
2025-09-28 16:01:13 +0530

    Add files via upload

---
+++ Accept 10 values and print 4th, 7th, 9th 24.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int arr[10];
+    for(int i=0;i<10;i++)
+        scanf("%d",&arr[i]);
+    printf("4th: %d, 7th: %d, 9th: %d\n", arr[3], arr[6], arr[8]);
+    return 0;
+}
