b5d764ba6c51c0374160c1b3cef0e63fbb979731
meghav11 <meghavpatwari11@gmail.com>
2025-09-28 16:08:03 +0530

    Add files via upload

--- Add two 2D arrays 24.c
+++ Add two 2D arrays 24.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 int main() {
-    int a[2][2], b[2][2], sum[2][2];
-    printf("Enter first matrix: ");
+    int a[2][2], b[2][2], c[2][2]={0};
     for(int i=0;i<2;i++)
         for(int j=0;j<2;j++) scanf("%d",&a[i][j]);
-    printf("Enter second matrix: ");
     for(int i=0;i<2;i++)
         for(int j=0;j<2;j++) scanf("%d",&b[i][j]);
     for(int i=0;i<2;i++)
         for(int j=0;j<2;j++)
-            sum[i][j] = a[i][j] + b[i][j];
-    printf("Sum matrix:\n");
+            for(int k=0;k<2;k++)
+                c[i][j]+=a[i][k]*b[k][j];
     for(int i=0;i<2;i++){
-        for(int j=0;j<2;j++) printf("%d ",sum[i][j]);
+        for(int j=0;j<2;j++) printf("%d ",c[i][j]);
         printf("\n");
     }
     return 0;
