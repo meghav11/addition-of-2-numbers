a8e506c19a932200483b236b255a69b09f77a264
meghav11 <meghavpatwari11@gmail.com>
2025-09-30 12:12:12 +0530

    Add files via upload

--- Delete a specific value 24.c
+++ Delete a specific value 24.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 int main() {
-    int arr[10], n=5, val;
-    for(int i=0;i<n;i++) scanf("%d",&arr[i]);
-    printf("Enter value to delete: "); scanf("%d",&val);
-    for(int i=0;i<n;i++){
-        if(arr[i]==val){
-            for(int j=i;j<n-1;j++) arr[j]=arr[j+1];
-            n--; i--;
-        }
-    }
-    for(int i=0;i<n;i++) printf("%d ",arr[i]);
+    int arr[10], val, found=0;
+    for(int i=0;i<10;i++) scanf("%d",&arr[i]);
+    printf("Enter value to search: "); scanf("%d",&val);
+    for(int i=0;i<10;i++)
+        if(arr[i]==val){ found=1; break; }
+    if(found) printf("Value found\n");
+    else printf("Value not found\n");
     return 0;
 }
